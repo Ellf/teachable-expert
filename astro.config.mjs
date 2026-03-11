627a5410a4bf3de7bf76f984d27634ea36470021
Tom Lorimer <tom@purplehippo.io>
2026-03-11 06:18:36 +0000

    feature: added GA Partytown to the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teachable.expert',
@@ -19,7 +21,7 @@ export default defineConfig({
     },
   },
 
-  integrations: [sitemap()],
+  integrations: [sitemap(), partytown()],
   redirects: {
     '/teachable-api': '/api-guides/teachable-api'
   }
